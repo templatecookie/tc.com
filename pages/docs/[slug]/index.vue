64f9367b77036dc2a60b4ea1d4fe8a7b02f035f6
Zakir Hossen <web.zakirbd@gmail.com>
2023-07-13 17:40:16 +0600

    fix: component import from content folder'

--- pages/docs/[slug]/index.vue
+++ pages/docs/[slug]/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex flex-wrap gap-4 ml-4 w-full">
-    <ContentCategoryCardItem :categories="$attrs.categories" />
+    <CategoryCardItem :categories="$attrs.categories" />
   </div>
 </template>
