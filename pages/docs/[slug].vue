4b65a7908e2c730a5a53fc0e73227d1a16d3d1e9
pro-dev-ryan <azizul.developer@gmail.com>
2023-07-14 10:26:15 +0600

    toc links[0] will be selected by default

--- pages/docs/[slug].vue
+++ pages/docs/[slug].vue
@@ -19,8 +19,8 @@
             <ul class="mb-6" v-for="(items, index) in categories" :key="index">
               <li class="uppercase text-xs text-gray-500 mb-2"> {{ index }} </li>
               <li v-for="(item, index) in items" :key="index">
-                <a :href="item._path" class="px-4 py-2 inline-block w-full text-gray-700 font-light text-sm"> {{
-                  item.title }} </a>
+                <NuxtLink :href="item._path" class="px-4 py-2 inline-block w-full text-gray-700 font-light text-sm"> {{
+                  item.title }} </NuxtLink>
               </li>
             </ul>
             <ul>
